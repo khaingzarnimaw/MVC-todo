7e66a3e3a96dd76d12745ef89a8e027254d47cc6
Khaing Maw <khaingmaw@Khaings-MacBook-Air.local>
2023-07-20 20:02:23 +0900

    client side update data addtodo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoList from "./components/TodoList";
 import CheckAllAndRemaining from "./components/CheckAllAndRemaining";
 import TodoFilters from "./components/TodoFilters";
 import ClearCompletedBtn from "./components/ClearCompletedBtn";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   let [todos, setTodos] = useState([]);
@@ -19,14 +19,21 @@ function App() {
       }, []);
   }, []);
 
+  //todo updat & api ကို delete လုပ်ဖို့လို
+  let addTodo = (todo) => {
+    //update data at server side
+
+    //update data at client side
+    setTodos(prevState => [...prevState,todo])
+  }
 
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
 
-        <TodoForm />
-        <TodoList todos={todos}/>
+        <TodoForm  addTodo={addTodo}/> 
+        <TodoList todos={todos} />
         <CheckAllAndRemaining />
         <div className="other-buttons-container">
           <TodoFilters />
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function TodoForm() {
+export default function TodoForm({addTodo}) {
+
+  //  todo တွေကို add လုပ်ရမယ်
+  let[title,setTitle]=useState('')
+
+  let handleSubmit = (e)=> {
+    e.preventDefault(); //page referch ကိုရပ််
+    // console.log("hit");
+
+    //todo object 
+    let todo ={
+      id : Math.random(),
+      title,
+      completed : false
+    };
+    addTodo(todo)//addTodo
+    setTitle('');//clear input
+
+  }
+
   return (
-    <form action="#">
+    <form action="#" onSubmit={handleSubmit}> 
+          {/* {title} */}
           <input
             type="text"
             className="todo-input"
             placeholder="What do you need to do?"
+            // useState နဲ့ data bind
+            onChange ={ e => setTitle(e.target.value)}
+            value={title}//tow way data binding
           />
         </form>
   )
